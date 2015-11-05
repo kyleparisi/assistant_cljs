47f0b855e65525b4232763bc079e33bd5986aae7
Kyle Parisi <kyle@getsidecar.com>
2015-11-04 20:38:13 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "A super awesome extensible personal assistant"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2280"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [cljs-http "0.1.15"]
                  [com.cognitect/transit-cljs "0.8.188"]
@@ -12,7 +12,8 @@
                  [garden "1.2.1"]
                  [prismatic/dommy "0.1.3"]
                  [hickory "0.5.4"]
-                 [org.omcljs/om "0.8.8"]]
+                 [org.omcljs/om "1.0.0-alpha14"]
+                 [figwheel-sidecar "0.4.0" :scope "provided"]]
 
   :plugins [[lein-cljsbuild "1.0.6-SNAPSHOT"]
             [lein-node-webkit-build "0.1.6"]]
